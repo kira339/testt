1e94584f5f5902de881ac40c8465314da4c61c85
Hiro Protagonist <shdesai327@gmail.com>
2023-06-05 12:16:42 -0400

    s

--- src/components/HistoryHeader.js
+++ src/components/HistoryHeader.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import "../index.css";
 
 export default function HistoryHeader() {
   const [selectedCategory, setSelectedCategory] = useState("All categories");
@@ -24,20 +25,17 @@ export default function HistoryHeader() {
   };
 
   return (
-    <div className="bg-primaryGray flex flex-col gap-4 p-8 items-center">
-      <h2 className="text-2xl font-bold mb-4 w-full text-center text-primaryBlack">
+    <div className="header-container">
+      <h2 className="header-title">
         Search Query: {searchQuery}, Number of Records: {recordCount}
       </h2>
 
-      <form
-        className="flex flex-wrap items-center justify-center space-x-4 w-full bg-primaryWhite"
-        onSubmit={handleSearch}
-      >
+      <form className="form-container" onSubmit={handleSearch}>
         {/* Category dropdown */}
-        <div className="relative w-full md:w-1/6 my-2 md:my-0">
+        <div className="dropdown-container">
           <select
             id="category-dropdown"
-            className="block w-full py-2.5 px-4 text-sm text-primaryBlack bg-primaryWhite border border-primaryBlack rounded-l-lg hover:border-primaryBlue focus:ring-primaryBlue focus:border-primaryBlue dark:bg-lightGray dark:border-primaryGray dark:placeholder-lightGray dark:text-primaryWhite dark:focus:border-primaryBlue appearance-none"
+            className="dropdown"
             value={selectedCategory}
             onChange={(e) => setSelectedCategory(e.target.value)}
           >
@@ -50,11 +48,11 @@ export default function HistoryHeader() {
         </div>
 
         {/* Search input */}
-        <div className="relative w-full md:w-1/6 my-2 md:my-0 md:ml-2">
+        <div className="input-container">
           <input
             type="search"
             id="search-input"
-            className="block p-2.5 w-full z-20 text-sm text-primaryBlack bg-primaryWhite border border-primaryBlack rounded-r-lg hover:border-primaryBlue focus:ring-primaryBlue focus:border-primaryBlue dark:bg-lightGray dark:border-primaryGray dark:placeholder-lightGray dark:text-primaryWhite dark:focus:border-primaryBlue"
+            className="input"
             placeholder={`Search ${selectedCategory}`}
             required
             onChange={(e) => setSearchQuery(e.target.value)}
@@ -62,14 +60,11 @@ export default function HistoryHeader() {
         </div>
 
         {/* Search button */}
-        <div className="w-full md:w-auto my-2 md:my-0 md:ml-2">
-          <button
-            type="submit"
-            className="bg-primaryBlue hover:bg-primaryOrange text-primaryWhite font-bold py-2 px-4 rounded flex items-center"
-          >
+        <div className="button-container">
+          <button type="submit" className="button">
             <svg
               aria-hidden="true"
-              className="w-5 h-5"
+              className="svg"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -87,20 +82,17 @@ export default function HistoryHeader() {
         </div>
 
         {/* Date picker */}
-        <div className="w-full md:w-1/6 my-2 md:my-0 md:ml-2">
+        <div className="input-container">
           <DatePicker
             selected={startDate}
             onChange={(date) => setStartDate(date)}
-            className="block w-full py-2.5 px-4 text-sm text-primaryBlack bg-primaryWhite border border-primaryBlack rounded-l-lg hover:border-primaryBlue focus:ring-primaryBlue focus:border-primaryBlue dark:bg-lightGray dark:border-primaryGray dark:placeholder-lightGray dark:text-primaryWhite dark:focus:border-primaryBlue appearance-none"
+            className="input"
           />
         </div>
 
         {/* Download button */}
-        <div className="w-full md:w-auto my-2 md:my-0 md:ml-2">
-          <button
-            type="submit"
-            className="bg-primaryBlue hover:bg-primaryOrange text-primaryWhite font-bold py-2 px-4 rounded"
-          >
+        <div className="button-container">
+          <button type="submit" className="button">
             Download
           </button>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,53 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         // Gray
+//         primaryGray: "#475569",
+//         // Orange
+//         primaryOrange: "#f59e0b",
+//         // Blue
+//         primaryBlue: "#3b82f6",
+//         // White
+//         primaryWhite: "#f8fafc",
+//         // Light gray
+//         lightGray: "#9ca3af",
+//         // Black
+//         primaryBlack: "#020617",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
   theme: {
     extend: {
       colors: {
-        // Gray
-        primaryGray: "#475569",
-        // Orange
-        primaryOrange: "#f59e0b",
-        // Blue
-        primaryBlue: "#3b82f6",
-        // White
-        primaryWhite: "#f8fafc",
-        // Light gray
-        lightGray: "#9ca3af",
-        // Black
-        primaryBlack: "#020617",
+        primaryGray: "#F3F4F6",
+        primaryBlack: "#111827",
+        primaryWhite: "#FFFFFF",
+        primaryBlue: "#3B82F6",
+        lightGray: "#D1D5DB",
+        primaryOrange: "#F97316",
+        primaryWhite: "#FFFFFF",
+        primaryGray: "#D1D5DB",
+        lightGray: "#D3DCE6",
+      },
+      borderRadius: {
+        "2xl": "2rem",
+      },
+      spacing: {
+        128: "32rem",
+        144: "36rem",
       },
     },
   },
+  variants: {},
   plugins: [],
 };
