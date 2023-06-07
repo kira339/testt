7104981500be4065709d65576347e0615fe6be59
Hiro Protagonist <shdesai327@gmail.com>
2023-06-07 09:34:06 -0400

    added

--- src/DB/historyData.js
+++ src/DB/historyData.js
@@ -1,17 +1,17 @@
 export const tableData = [
   {
-    account: "007200685801",
-    contact: "10000000",
+    account: "007244485801",
+    contact: "50000000",
     alertType: "Registration_Confirmation",
     deliveryMethod: "SMS",
     dateTime: "03/20/2023 11:28:25 AM",
     deliveryStatus: "Bounce",
     actualMessage:
-      "PSE&G: Text Y to activate your registration for the account at Franklin Ave. Msg&Data rates may apply. Text Stop to cancel, HELP for help",
+      "test: Text Y to activate your registration for the account at hello Ave. Msg&Data rates may apply. Text Stop to cancel, HELP for help",
   },
   {
     account: "007200685802",
-    contact: "10000001",
+    contact: "40000001",
     alertType: "Password_Reset",
     deliveryMethod: "Email",
     dateTime: "03/21/2023 10:15:00 AM",
@@ -20,7 +20,7 @@ export const tableData = [
       "Hello, please use this link to reset your password: www.example.com/reset",
   },
   {
-    account: "007200685803",
+    account: "007255585803",
     contact: "10000002",
     alertType: "Billing_Notification",
     deliveryMethod: "Phone",
@@ -30,8 +30,8 @@ export const tableData = [
       "Your account is overdue. Please pay your balance immediately to avoid service interruption.",
   },
   {
-    account: "007200685804",
-    contact: "10000003",
+    account: "007200665804",
+    contact: "30000003",
     alertType: "Appointment_Reminder",
     deliveryMethod: "SMS",
     dateTime: "03/23/2023 4:45:10 PM",
