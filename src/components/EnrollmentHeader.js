272205f06e3d2ba6b1976c971985a988107d4020
Hiro Protagonist <shdesai327@gmail.com>
2023-06-10 09:29:16 -0400

    f

--- src/components/EnrollmentHeader.js
+++ src/components/EnrollmentHeader.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-function EnrollmentHeader({ searchInput, setSearchInput, handleSearch }) {
+const EnrollmentHeader = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    onSearch(searchTerm);
+  };
+
   return (
     <div className="enrollment-header">
       <h1>Enrollments</h1>
-      <input
-        type="text"
-        value={searchInput}
-        onChange={(e) => setSearchInput(e.target.value)}
-      />
-      <button onClick={handleSearch}>Search</button>
+      <form onSubmit={handleSearchSubmit}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          placeholder="Search..."
+          className="search-input"
+        />
+        <button type="submit" className="button">
+          Search
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default EnrollmentHeader;
